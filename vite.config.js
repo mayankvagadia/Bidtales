7dfa7f79333ce002973920a06d3c15f258114ec1
mayankvagadia17 <mayankvagadia007@gmail.com>
2025-05-12 13:00:10 +0530

    host allowed

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: 5173, // Use Render's provided port or fallback to 5173
+    port: 5173,
+    allowedHosts: ['bidtales.onrender.com'],
   }
 })
